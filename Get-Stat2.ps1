38523ea0c60f4cc64b5f4da976fe13cef3424a2e
SBT-Nevedomskiy-AA <sbt-nevedomskiy-aa@mail.ca.sbrf.ru>
2015-04-13 11:19:03 +0500

    Updated function to support multiple entitites

--- Get-Stat2.ps1
+++ Get-Stat2.ps1
@@ -1,35 +1,45 @@
 function Get-Stat2 {
-  <#
-.SYNOPSIS  Retrieve vSphere statistics
-.DESCRIPTION The function is an alternative to the Get-Stat cmdlet.
-  It's primary use is to provide functionality that is missing
-  from the Get-Stat cmdlet.
-.NOTES  Author:  Luc Dekens
-.PARAMETER Entity
-  Specify the VIObject for which you want to retrieve statistics
-  This needs to be an SDK object
-.PARAMETER Start
-  Start of the interval for which to retrive statistics
-.PARAMETER Finish
-  End of the interval for which to retrive statistics
-.PARAMETER Stat
-  The identifiers of the metrics to retrieve
-.PARAMETER Instance
-  The instance property of the statistics to retrieve
-.PARAMETER Interval
-  Specify for which interval you want to retrieve statistics.
-  Allowed values are RT, HI1, HI2, HI3 and HI4
-.PARAMETER MaxSamples
-  The maximum number of samples for each metric
-.PARAMETER QueryMetrics
-  Switch to indicate that the function should return the available
-  metrics for the Entity specified
-.PARAMETER QueryInstances
-  Switch to indicate that the function should return the valid instances
-  for a specific Entity and Stat
-.EXAMPLE
-  PS> Get-Stat2 -Entity $vm.Extensiondata -Stat "cpu.usage.average" -Interval "RT"
-#>
+    <#
+    .Synopsis
+       Retrieve vSphere statistics
+    .DESCRIPTION
+       The function is an alternative to the Get-Stat cmdlet.
+       It's primary use is to provide functionality that is missing
+       from the Get-Stat cmdlet.
+    .EXAMPLE
+       PS> Get-Stat2 -Entity $vm.Extensiondata -Stat "cpu.usage.average" -Interval "RT"
+    .PARAMETER Entity
+      Specify the VIObject(s) for which you want to retrieve statistics
+      This needs to be an SDK object
+    .PARAMETER Start
+      Start of the interval for which to retrive statistics
+    .PARAMETER Finish
+      End of the interval for which to retrive statistics
+    .PARAMETER Stat
+      The identifiers of the metrics to retrieve
+    .PARAMETER Instance
+      The instance property of the statistics to retrieve
+    .PARAMETER Interval
+      Specify for which interval you want to retrieve statistics.
+      Allowed values are RT, HI1, HI2, HI3 and HI4
+    .PARAMETER MaxSamples
+      The maximum number of samples for each metric
+    .PARAMETER QueryMetrics
+      Switch to indicate that the function should return the available
+      metrics for the Entity specified
+    .PARAMETER QueryInstances
+      Switch to indicate that the function should return the valid instances
+      for a specific Entity and Stat
+    .NOTES
+       Original author:  Luc Dekens
+       Modification author:  Andrey Nevedomskiy
+
+       Updated 13.04.2015 by Nevedomskiy Andrey (monosoul):
+       Added ability to get metrics for multiple entities at a time and parallel parsing.
+    .FUNCTIONALITY
+       Retrieve vSphere statistics
+   
+    #>
 
   [CmdletBinding()]
   param (
@@ -45,191 +55,205 @@ function Get-Stat2 {
     [switch]$QueryMetrics,
     [switch]$QueryInstances)
 
-  # Test if entity is valid
-  $EntityType = $Entity.GetType().Name
+  $speclist = @()
 
-  if(!(("HostSystem",
-        "VirtualMachine",
-        "ClusterComputeResource",
-        "Datastore",
-        "ResourcePool") -contains $EntityType)) {
-    Throw "-Entity parameters should be of type HostSystem, VirtualMachine, ClusterComputeResource, Datastore or ResourcePool"
-  }
+  ForEach ($item in @($Entity)) {
 
-  $perfMgr = Get-View (Get-View ServiceInstance).content.perfManager
-
-  # Create performance counter hashtable
-  $pcTable = New-Object Hashtable
-  $keyTable = New-Object Hashtable
-  foreach($pC in $perfMgr.PerfCounter){
-    if($pC.Level -ne 99){
-      if(!$pctable.containskey($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType)){
-        $pctable.Add(($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType),$pC.Key)
-        $keyTable.Add($pC.Key, $pC)
+    # Test if entity is valid
+    Remove-Variable -Name EntityType -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
+    $EntityType = $item.GetType().Name
+  
+    if(!(("HostSystem",
+          "VirtualMachine",
+          "ClusterComputeResource",
+          "Datastore",
+          "ResourcePool") -contains $EntityType)) {
+      Throw "-Entity parameters should be of type HostSystem, VirtualMachine, ClusterComputeResource, Datastore or ResourcePool"
+    }
+  
+    $perfMgr = Get-View (Get-View ServiceInstance).content.perfManager
+  
+    # Create performance counter hashtable
+    $pcTable = New-Object Hashtable
+    $keyTable = New-Object Hashtable
+    foreach($pC in $perfMgr.PerfCounter){
+      if($pC.Level -ne 99){
+        if(!$pctable.containskey($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType)){
+          $pctable.Add(($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType),$pC.Key)
+          $keyTable.Add($pC.Key, $pC)
+        }
       }
     }
-  }
-
-  # Test for a valid $Interval
-  if($Interval.ToString().Split(" ").count -gt 1){
-    Throw "Only 1 interval allowed."
-  }
-
-  $intervalTab = @{"RT"=$null;"HI1"=0;"HI2"=1;"HI3"=2;"HI4"=3}
-  $dsValidIntervals = "HI2","HI3","HI4"
-  $intervalIndex = $intervalTab[$Interval]
-
-  if($EntityType -ne "datastore"){
-    if($Interval -eq "RT"){
-      $numinterval = 20
+  
+    # Test for a valid $Interval
+    if($Interval.ToString().Split(" ").count -gt 1){
+      Throw "Only 1 interval allowed."
     }
-    else{
-      $numinterval = $perfMgr.HistoricalInterval[$intervalIndex].SamplingPeriod
-    }
-  }
-  else{
-    if($dsValidIntervals -contains $Interval){
-      $numinterval = $null
-      if(!$Start){
-        $Start = (Get-Date).AddSeconds($perfMgr.HistoricalInterval[$intervalIndex].SamplingPeriod - $perfMgr.HistoricalInterval[$intervalIndex].Length)
+  
+    $intervalTab = @{"RT"=$null;"HI1"=0;"HI2"=1;"HI3"=2;"HI4"=3}
+    $dsValidIntervals = "HI2","HI3","HI4"
+    $intervalIndex = $intervalTab[$Interval]
+  
+    if($EntityType -ne "datastore"){
+      if($Interval -eq "RT"){
+        $numinterval = 20
       }
-      if(!$Finish){
-        $Finish = Get-Date
+      else{
+        $numinterval = $perfMgr.HistoricalInterval[$intervalIndex].SamplingPeriod
       }
     }
     else{
-      Throw "-Interval parameter $Interval is invalid for datastore metrics."
-    }
-  }
-
-  # Test if QueryMetrics is given
-  if($QueryMetrics){
-    $metrics = $perfMgr.QueryAvailablePerfMetric($Entity.MoRef,$null,$null,$numinterval)
-    $metricslist = @()
-    foreach($pmId in $metrics){
-      $pC = $keyTable[$pmId.CounterId]
-      $metricslist += New-Object PSObject -Property @{
-        Group = $pC.GroupInfo.Key
-        Name = $pC.NameInfo.Key
-        Rollup = $pC.RollupType
-        Id = $pC.Key
-        Level = $pC.Level
-        Type = $pC.StatsType
-        Unit = $pC.UnitInfo.Key
+      if($dsValidIntervals -contains $Interval){
+        $numinterval = $null
+        if(!$Start){
+          $Start = (Get-Date).AddSeconds($perfMgr.HistoricalInterval[$intervalIndex].SamplingPeriod - $perfMgr.HistoricalInterval[$intervalIndex].Length)
+        }
+        if(!$Finish){
+          $Finish = Get-Date
+        }
+      }
+      else{
+        Throw "-Interval parameter $Interval is invalid for datastore metrics."
       }
     }
-    return ($metricslist | Sort-Object -unique -property Group,Name,Rollup)
-  }
-
-  # Test if start is valid
-  if($Start -ne $null -and $Start -ne ""){
-    if($Start.gettype().name -ne "DateTime") {
-      Throw "-Start parameter should be a DateTime value"
+  
+    # Test if QueryMetrics is given
+    if($QueryMetrics){
+      $metrics = $perfMgr.QueryAvailablePerfMetric($item.MoRef,$null,$null,$numinterval)
+      $metricslist = @()
+      foreach($pmId in $metrics){
+        $pC = $keyTable[$pmId.CounterId]
+        $metricslist += New-Object PSObject -Property @{
+          Group = $pC.GroupInfo.Key
+          Name = $pC.NameInfo.Key
+          Rollup = $pC.RollupType
+          Id = $pC.Key
+          Level = $pC.Level
+          Type = $pC.StatsType
+          Unit = $pC.UnitInfo.Key
+        }
+      }
+      return ($metricslist | Sort-Object -unique -property Group,Name,Rollup)
     }
-  }
-
-  # Test if finish is valid
-  if($Finish -ne $null -and $Finish -ne ""){
-    if($Finish.gettype().name -ne "DateTime") {
-      Throw "-Start parameter should be a DateTime value"
+  
+    # Test if start is valid
+    if($Start -ne $null -and $Start -ne ""){
+      if($Start.gettype().name -ne "DateTime") {
+        Throw "-Start parameter should be a DateTime value"
+      }
     }
-  }
-
-  # Test start-finish interval
-  if($Start -ne $null -and $Finish -ne $null -and $Start -ge $Finish){
-    Throw "-Start time should be 'older' than -Finish time."
-  }
-
-  # Test if stat is valid
-  $unitarray = @()
-  $InstancesList = @()
-
-  foreach($st in $Stat){
-    if($pcTable[$st] -eq $null){
-      Throw "-Stat parameter $st is invalid."
+  
+    # Test if finish is valid
+    if($Finish -ne $null -and $Finish -ne ""){
+      if($Finish.gettype().name -ne "DateTime") {
+        Throw "-Start parameter should be a DateTime value"
+      }
     }
-    $pcInfo = $perfMgr.QueryPerfCounter($pcTable[$st])
-    $unitarray += $pcInfo[0].UnitInfo.Key
-    $metricId = $perfMgr.QueryAvailablePerfMetric($Entity.MoRef,$null,$null,$numinterval)
-
-    # Test if QueryInstances in given
-    if($QueryInstances){
-      $mKey = $pcTable[$st]
-      foreach($metric in $metricId){
-        if($metric.CounterId -eq $mKey){
-          $InstancesList += New-Object PSObject -Property @{
-            Stat = $st
-            Instance = $metric.Instance
+  
+    # Test start-finish interval
+    if($Start -ne $null -and $Finish -ne $null -and $Start -ge $Finish){
+      Throw "-Start time should be 'older' than -Finish time."
+    }
+  
+    # Test if stat is valid
+    Remove-Variable -Name unitarray -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
+    Remove-Variable -Name InstancesList -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
+    Remove-Variable -Name validInstances -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
+    Remove-Variable -Name st -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
+    $unitarray = @()
+    $InstancesList = @()
+  
+    foreach($st in $Stat){
+      if($pcTable[$st] -eq $null){
+        Throw "-Stat parameter $st is invalid."
+      }
+      $pcInfo = $perfMgr.QueryPerfCounter($pcTable[$st])
+      $unitarray += $pcInfo[0].UnitInfo.Key
+      $metricId = $perfMgr.QueryAvailablePerfMetric($item.MoRef,$null,$null,$numinterval)
+  
+      # Test if QueryInstances in given
+      if($QueryInstances){
+        $mKey = $pcTable[$st]
+        foreach($metric in $metricId){
+          if($metric.CounterId -eq $mKey){
+            $InstancesList += New-Object PSObject -Property @{
+              Stat = $st
+              Instance = $metric.Instance
+            }
           }
         }
       }
-    }
-    else{
-      # Test if instance is valid
-      $found = $false
-      $validInstances = @()
-      foreach($metric in $metricId){
-        if($metric.CounterId -eq $pcTable[$st]){
-          if($metric.Instance -eq "") {$cInstance = '""'} else {$cInstance = $metric.Instance}
-          $validInstances += $cInstance
-          if($Instance -eq $metric.Instance){$found = $true}
+      else{
+        # Test if instance is valid
+        $found = $false
+        $validInstances = @()
+        foreach($metric in $metricId){
+          if($metric.CounterId -eq $pcTable[$st]){
+            if($metric.Instance -eq "") {$cInstance = '""'} else {$cInstance = $metric.Instance}
+            $validInstances += $cInstance
+            if($Instance -eq $metric.Instance){$found = $true}
+          }
+        }
+        if(!$found){
+          Throw "-Instance parameter invalid for requested stat: $st.`nValid values are: $validInstances"
         }
       }
-      if(!$found){
-        Throw "-Instance parameter invalid for requested stat: $st.`nValid values are: $validInstances"
+    }
+    if($QueryInstances){
+      return $InstancesList
+    }
+  
+    $PQSpec = New-Object VMware.Vim.PerfQuerySpec
+    $PQSpec.entity = $item.MoRef
+    $PQSpec.Format = "normal"
+    $PQSpec.IntervalId = $numinterval
+    $PQSpec.MetricId = @()
+    foreach($st in $Stat){
+      $PMId = New-Object VMware.Vim.PerfMetricId
+      $PMId.counterId = $pcTable[$st]
+      if($Instance -ne $null){
+        $PMId.instance = $Instance
       }
+      $PQSpec.MetricId += $PMId
     }
-  }
-  if($QueryInstances){
-    return $InstancesList
-  }
-
-  $PQSpec = New-Object VMware.Vim.PerfQuerySpec
-  $PQSpec.entity = $Entity.MoRef
-  $PQSpec.Format = "normal"
-  $PQSpec.IntervalId = $numinterval
-  $PQSpec.MetricId = @()
-  foreach($st in $Stat){
-    $PMId = New-Object VMware.Vim.PerfMetricId
-    $PMId.counterId = $pcTable[$st]
-    if($Instance -ne $null){
-      $PMId.instance = $Instance
+    if ($Start) { $PQSpec.StartTime = $Start }
+    if ($Finish) { $PQSpec.EndTime = $Finish }
+    if($MaxSamples -eq 0 -or $numinterval -eq 20){
+      $PQSpec.maxSample = $null
     }
-    $PQSpec.MetricId += $PMId
+    else{
+      $PQSpec.MaxSample = $MaxSamples
+    }
+    $speclist += $PQSpec
   }
-  $PQSpec.StartTime = $Start
-  $PQSpec.EndTime = $Finish
-  if($MaxSamples -eq 0 -or $numinterval -eq 20){
-    $PQSpec.maxSample = $null
-  }
-  else{
-    $PQSpec.MaxSample = $MaxSamples
-  }
-  $Stats = $perfMgr.QueryPerf($PQSpec)
+  $Stats = $perfMgr.QueryPerf($speclist)
 
   # No data available
   if($Stats[0].Value -eq $null) {return $null}
 
   # Extract data to custom object and return as array
   $data = @()
-  for($i = 0; $i -lt $Stats[0].SampleInfo.Count; $i ++ ){
-    for($j = 0; $j -lt $Stat.Count; $j ++ ){
-      $data += New-Object PSObject -Property @{
-        CounterId = $Stats[0].Value[$j].Id.CounterId
-        CounterName = $Stat[$j]
-        Instance = $Stats[0].Value[$j].Id.Instance
-        Timestamp = $Stats[0].SampleInfo[$i].Timestamp
-        Interval = $Stats[0].SampleInfo[$i].Interval
-        Value = &{if($Stats[0].Value){
-                  $Stats[0].Value[$j].Value[$i]
-                  }else{-1}
-                }
-        Unit = $unitarray[$j]
-        Entity = $Entity.Name
-        EntityId = $Entity.MoRef.ToString()
+  ForEach ($stats_item in $stats) {
+    for($i = 0; $i -lt $stats_item.SampleInfo.Count; $i ++ ){
+      for($j = 0; $j -lt $Stat.Count; $j ++ ){
+        $data += New-Object PSObject -Property @{
+          CounterId = $stats_item.Value[$j].Id.CounterId
+          CounterName = $Stat[$j]
+          Instance = $stats_item.Value[$j].Id.Instance
+          Timestamp = $stats_item.SampleInfo[$i].Timestamp
+          Interval = $stats_item.SampleInfo[$i].Interval
+          Value = &{if($stats_item.Value){
+              $stats_item.Value[$j].Value[$i]
+            }else{-1}
+          }
+          Unit = $unitarray[$j]
+          Entity = ($Entity | Where-Object { $_.MoRef -eq $stats_item.Entity }).Name
+          EntityId = $stats_item.Entity.ToString()
+        }
       }
     }
   }
+
   if($MaxSamples -eq 0){
     $data | Sort-Object -Property Timestamp -Descending
   }
