31eb4a042b47bc0722478042462065a637837efd
monosoul <Kloz.Klaud@gmail.com>
2015-10-16 11:58:50 +0500

    Get-Stat2.ps1: Update

--- Get-Stat2.ps1
+++ Get-Stat2.ps1
@@ -30,6 +30,10 @@ function Get-Stat2 {
     .PARAMETER QueryInstances
       Switch to indicate that the function should return the valid instances
       for a specific Entity and Stat
+    .PARAMETER threads
+      Amount of threads to use. Should be less or equal to number of logical CPUs multiplied by 2.
+    .PARAMETER SkipInstanceCheck
+      Skip instances validation.
     .NOTES
        Original author:  Luc Dekens
        Modification author:  Andrey Nevedomskiy
@@ -39,6 +43,9 @@ function Get-Stat2 {
        - Added parallel results parsing via runspaces.
        Updated 22.04.2015 by Nevedomskiy Andrey (monosoul):
        - Optimized parallel output parsing, now it would be up to 2x faster.
+       Updated 16.10.2015 by Nevedomskiy Andrey (monosoul):
+       - Added ability to get metrics for multiple instances at a time.
+       - Added ability to use asterisk (*) as instance like in Get-Stat by VMware.
     .FUNCTIONALITY
        Retrieve vSphere statistics
    
@@ -51,24 +58,28 @@ function Get-Stat2 {
     [DateTime]$Start,
     [DateTime]$Finish,
     [String[]]$Stat,
-    [String]$Instance = "",
+    [String[]]$Instance = "",
     [ValidateSet("RT","HI1","HI2","HI3","HI4")]
     [String]$Interval = "RT",
     [int]$MaxSamples,
     [switch]$QueryMetrics,
-    [switch]$QueryInstances)
+    [switch]$QueryInstances,
+    [int]$threads,
+    [switch]$SkipInstanceCheck)
 
   $speclist = @()
 
   $perfMgr = Get-View (Get-View ServiceInstance).content.perfManager
 
   # Create performance counter hashtable
-  $pcTable = New-Object Hashtable
-  $keyTable = New-Object Hashtable
+  $pcTable    = New-Object Hashtable
+  $pcTableRev = New-Object Hashtable
+  $keyTable   = New-Object Hashtable
   foreach($pC in $perfMgr.PerfCounter){
     if($pC.Level -ne 99){
       if(!$pctable.containskey($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType)){
         $pctable.Add(($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType),$pC.Key)
+        $pcTableRev.Add($pC.Key,($pC.GroupInfo.Key + "." + $pC.NameInfo.Key + "." + $pC.RollupType))
         $keyTable.Add($pC.Key, $pC)
       }
     }
@@ -103,13 +114,13 @@ function Get-Stat2 {
   }
 
   # Building unit array
-  $unitarray = @()
+  $unitarray = New-Object Hashtable
   foreach($st in $Stat){
     if($pcTable[$st] -eq $null){
       Throw "-Stat parameter $st is invalid."
     }
     $pcInfo = $perfMgr.QueryPerfCounter($pcTable[$st])
-    $unitarray += $pcInfo[0].UnitInfo.Key
+    $unitarray.add($pcTable[$st],$pcInfo[0].UnitInfo.Key)
   }
 
   ForEach ($item in @($Entity)) {
@@ -172,7 +183,7 @@ function Get-Stat2 {
     }
   
     #if passed more than one entities then skip stat check to speed up process
-    if (@($Entity).Count -eq 1) {
+    if (!$SkipInstanceCheck.IsPresent) {
       # Test if stat is valid
       if ($InstancesList) {
         Remove-Variable -Name InstancesList -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Confirm:$false
@@ -204,15 +215,33 @@ function Get-Stat2 {
           # Test if instance is valid
           $found = $false
           $validInstances = @()
+          $givenInstances = {$Instance}.Invoke()
           foreach($metric in $metricId){
             if($metric.CounterId -eq $pcTable[$st]){
               if($metric.Instance -eq "") {$cInstance = '""'} else {$cInstance = $metric.Instance}
               $validInstances += $cInstance
-              if($Instance -eq $metric.Instance){$found = $true}
+              if($givenInstances.Contains($metric.Instance)){
+                $givenInstances.Remove($metric.Instance) | Out-Null
+              }
             }
           }
+          if($givenInstances.Contains("*")){
+            $givenInstances.Remove("*") | Out-Null
+          }
+          if ($givenInstances.Count -eq 0) {$found = $true}
           if(!$found){
-            Throw "-Instance parameter invalid for requested stat: $st.`nValid values are: $validInstances"
+            [string]$stringInstance = ""
+            $givenInstances | %{
+              $stringInstance += $_.ToString() + ", "
+            }
+            $stringInstance = $stringInstance -replace ", $",""
+            Throw "-Instance parameter ($stringInstance) invalid for requested stat: $st.`nValid values are: $validInstances"
+          }
+          if ($stringInstance) {
+            Remove-Variable -Name stringInstance -Force -Confirm:$false
+          }
+          if ($givenInstances) {
+            Remove-Variable -Name givenInstances -Force -Confirm:$false
           }
         }
       }
@@ -227,12 +256,18 @@ function Get-Stat2 {
     $PQSpec.IntervalId = $numinterval
     $PQSpec.MetricId = @()
     foreach($st in $Stat){
-      $PMId = New-Object VMware.Vim.PerfMetricId
-      $PMId.counterId = $pcTable[$st]
-      if($Instance -ne $null){
-        $PMId.instance = $Instance
+      if (!([System.Array]::Equals($Instance, $null))) {
+        $Instance | ForEach-Object {
+          $PMId = New-Object VMware.Vim.PerfMetricId
+          $PMId.counterId = $pcTable[$st]
+          $PMId.instance = $_
+          $PQSpec.MetricId += $PMId
+        }
+      } else {
+        $PMId = New-Object VMware.Vim.PerfMetricId
+        $PMId.counterId = $pcTable[$st]
+        $PQSpec.MetricId += $PMId
       }
-      $PQSpec.MetricId += $PMId
     }
     if ($Start) { $PQSpec.StartTime = $Start }
     if ($Finish) { $PQSpec.EndTime = $Finish }
@@ -257,6 +292,9 @@ function Get-Stat2 {
   }
   # setting throttle to amount of logical CPUs * 2
   $throttle = ($cpunum * 2)
+  if ($threads -and ($threads -le ($throttle))) {
+    $throttle = $threads
+  }
 
   # getting amount of stats per thread
   $limit = [math]::Ceiling(($Stats.Count / $throttle))
@@ -272,6 +310,9 @@ function Get-Stat2 {
     $skip += $limit
   }
 
+  Remove-Variable -Name Stats -Force -Confirm:$false
+  [System.GC]::Collect()
+
   # parallelizing output parsing
   $RunspacePool = [RunspaceFactory]::CreateRunspacePool(1, $throttle)
   $RunspacePool.Open()
@@ -279,21 +320,21 @@ function Get-Stat2 {
   $RS_scriptblock = {
     param(
       [PSObject]$stats,
-      [PSObject]$Stat,
+      [PSObject]$pcTableRev,
       [PSObject]$unitarray
     )
     $data = @()
     ForEach ($stats_item in $stats) {
-      for($j = 0; $j -lt $Stat.Count; $j ++ ){
+      for($j = 0; $j -lt $stats_item.Value.Count; $j ++ ){
         $script:valuecounter = 0
         $data += $stats_item.Value[$j].Value | Select `
           @{N="Timestamp";E={$stats_item.SampleInfo[$script:valuecounter].Timestamp}},`
           @{N="Interval";E={$stats_item.SampleInfo[$script:valuecounter].Interval}},`
           @{N="Value";E={$_;$script:valuecounter++}},`
-          @{N="CounterName";E={$Stat[$j]}},`
+          @{N="CounterName";E={$pcTableRev[$stats_item.Value[$j].Id.CounterId]}},`
           @{N="CounterId";E={$stats_item.Value[$j].Id.CounterId}},`
           @{N="Instance";E={$stats_item.Value[$j].Id.Instance}},`
-          @{N="Unit";E={$unitarray[$j]}},`
+          @{N="Unit";E={$unitarray[$stats_item.Value[$j].Id.CounterId]}},`
           @{N="Entity";E={$stats_item.EntityName}},`
           @{N="EntityId";E={$stats_item.Entity.ToString()}}
       }
@@ -307,7 +348,7 @@ function Get-Stat2 {
     $Job = [powershell]::Create().AddScript($RS_scriptblock)
 
     $Job.AddArgument($group.Value) | Out-Null
-    $Job.AddArgument($Stat) | Out-Null
+    $Job.AddArgument($pcTableRev) | Out-Null
     $Job.AddArgument($unitarray) | Out-Null
 
     $Job.RunspacePool = $RunspacePool
@@ -317,7 +358,6 @@ function Get-Stat2 {
     })) | Out-Null
   }
 
-  Remove-Variable -Name Stats -Force -Confirm:$false
   Remove-Variable -Name group -Force -Confirm:$false
   Remove-Variable -Name groups -Force -Confirm:$false
   Remove-Variable -Name limit -Force -Confirm:$false
